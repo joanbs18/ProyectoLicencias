db22ed7024d5b51589f6d737fc050c0c61685492
Francisco <Francisco@Laptop-Fran>
2022-12-02 16:26:58 -0600

    se hacen cambios en daos

--- src/MVC/Modelos/Secretaria.java
+++ src/MVC/Modelos/Secretaria.java
@@ -8,12 +8,44 @@ package MVC.Modelos;
  *
  * @author joans
  */
-public class Secretaria extends Usuario{
-
-    public Secretaria(Integer Carnet, Integer IdPersona, String Contraseña) {
-        super(Carnet, IdPersona, Contraseña);
+public class Secretaria extends Persona implements Modelo{
+    private Integer id;
+    private String nombreUsuario;
+    private String password;
+    
+    public Secretaria(Integer cedula, String nombreCompleto, String fechaNacimiento, String email, String telefono) {
+        super(cedula, nombreCompleto, fechaNacimiento, email, telefono);
     }
 
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
+    @Override
+    public boolean isComplete() {
+         return this.nombreUsuario!=null && this.password!=null;
+    }
     
 }
